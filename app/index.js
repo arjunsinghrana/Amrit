68323a282f2cefd7bc5cd72ad7be4d3662edee6e
Arjun <asingh105@jaguarlandrover.com>
2018-07-26 14:16:05 +0100

    Add functionality to synchronise chains.

--- app/index.js
+++ app/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
 const bodyParser = require('body-parser');
+const P2PServer = require('./p2p-server')
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
+const p2pServer = new P2PServer(bc);
 
 app.use(bodyParser.json());
 
@@ -17,9 +19,13 @@ app.post('/mine', (request, response) => {
     const block = bc.addBlock(request.body.data);
     console.log(`New block added: ${block.toString()}`);
 
+    p2pServer.syncChains();
+
     response.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => {
     console.log(`Listening on port ${HTTP_PORT}`)
 });
+
+p2pServer.listen();
