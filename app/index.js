eda90343c683ca91cbb1aad4d24957a086dc39d2
Arjun <asingh105@jaguarlandrover.com>
2018-07-26 13:25:29 +0100

    Add mine/post request

--- app/index.js
+++ app/index.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const Blockchain = require('../blockchain');
+const bodyParser = require('body-parser');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
 const bc = new Blockchain();
 
+app.use(bodyParser.json());
+
 app.get('/blocks', (request, response) => {
     response.json(bc.chain);
+});
+
+app.post('/mine', (request, response) => {
+    const block = bc.addBlock(request.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    response.redirect('/blocks');
 });
 
 app.listen(HTTP_PORT, () => {
